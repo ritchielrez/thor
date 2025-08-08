d56769787d0fe4e07de93f4b61ee03f9c2f276ff
RitchielRez <ritchielrez@gmail.com>
2025-08-08 11:24:54 -0400

    feat(defines): add `nullptr` and `INTERNAL_DEF `

--- include/defines.h
+++ include/defines.h
@@ -1,9 +1,11 @@
 #ifndef DEFINES_H_INCLUDED
 #define DEFINES_H_INCLUDED
 
-#ifndef BIN_NAME_MAX_SZ
 #define BIN_NAME_MAX_SZ 16
-#endif  // BIN_NAME_MAX_SZ
+
+#define nullptr (void *)0
+
+#define INTERNAL_DEF static
 
 #if defined(_WIN32) || defined(__CYGWIN__)
 // This directive tells the Windows headers to exclude some of the less
@@ -13,12 +15,12 @@
 #include <windows.h>
 #elif defined(__linux__)
 #define BUILD_LINUX
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 #else
 #error Unknown operating system, only Windows and Linux is currently supported
 #endif
