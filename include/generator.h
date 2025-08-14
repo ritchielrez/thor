f60d2ba8af979796758dc0570d7c6448a7db56e8
RitchielRez <ritchielrez@gmail.com>
2025-08-14 12:06:58 -0400

    feat(generator): `generator()` can take a `nullptr`
    The default output file now is `out.c`.

--- include/generator.h
+++ include/generator.h
@@ -3,11 +3,16 @@
 
 #include <stdio.h>
 
+#include "defines.h"
 #include "libraries/rit_dyn_arr.h"
 #include "parser.h"
 
 static inline void generator(const char *t_file, node_prg *prg) {
-  FILE *file = fopen(t_file, "w");
+  const char *file_name = "out.c";
+  if (t_file != nullptr) {
+    file_name = t_file;
+  }
+  FILE *file = fopen(file_name, "w");
   fprintf(file, "#include <stdio.h>\n");
   fprintf(file, "int main() {\n");
   fprintf(file, "\texit(%I64d);\n", rda_at((*prg), 0).value.stmt_exit.status);
