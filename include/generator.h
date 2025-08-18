b5f881f540a6f117d16bb78eefd42689aa91947d
RitchielRez <ritchielrez@gmail.com>
2025-08-18 13:19:50 -0400

    feat(generator): go through all statements in a program and generate

--- include/generator.h
+++ include/generator.h
@@ -7,15 +7,32 @@
 #include "libraries/rit_dyn_arr.h"
 #include "parser.h"
 
-static inline void generator(const char *t_file, node_prg *prg) {
+/// @internal
+INTERNAL_DEF inline void generate_stmt_exit(FILE *t_file,
+                                            node_stmt_exit *t_stmt) {
+  fprintf(t_file, "\texit(%I64d);\n", t_stmt->status);
+}
+
+static inline void generator(const char *t_file_name, node_prg *prg) {
   const char *file_name = "out.c";
-  if (t_file != nullptr) {
-    file_name = t_file;
+  if (t_file_name != nullptr) {
+    file_name = t_file_name;
   }
   FILE *file = fopen(file_name, "w");
   fprintf(file, "#include <stdio.h>\n");
   fprintf(file, "int main() {\n");
-  fprintf(file, "\texit(%I64d);\n", rda_at((*prg), 0).value.stmt_exit.status);
+  rda_for_each(it, (*prg)) {
+    switch (it->type) {
+      case stmt_exit: {
+        generate_stmt_exit(file, &(it->value.stmt_exit));
+        break;
+      }
+      default: {
+        fprintf(stderr, "Error: unknown statment type\n");
+        exit(1);
+      }
+    }
+  }
   fprintf(file, "}\n");
   fclose(file);
 }
