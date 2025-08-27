72bf9dbc38f4e7101746e2bd6d6c50d474c4c157
RitchielRez <ritchielrez@gmail.com>
2025-08-27 11:59:09 -0400

    feat(generator): add a generator for expressions

--- include/generator.h
+++ include/generator.h
@@ -8,12 +8,30 @@
 #include "parser.h"
 
 /// @internal
-INTERNAL_DEF inline void generate_stmt_exit(FILE *t_file,
-                                            node_stmt_exit *t_stmt) {
-  fprintf(t_file, "\texit(%I64d);\n", t_stmt->status);
+INTERNAL_DEF inline void generate_expr(FILE *t_file, node_expr *t_expr) {
+  switch (t_expr->type) {
+    case expr_num: {
+      fprintf(t_file, "%I64d", t_expr->value.num_expr.num);
+      break;
+    }
+    case expr_bin: {
+      generate_expr(t_file, t_expr->value.bin_expr.lhs);
+      fprintf(t_file, "%s", token_type_to_str(t_expr->value.bin_expr.op));
+      generate_expr(t_file, t_expr->value.bin_expr.rhs);
+      break;
+    }
+  }
 }
 
-static inline void generator(const char *t_file_name, node_prg *prg) {
+/// @internal
+INTERNAL_DEF inline void generate_stmt_exit(FILE *t_file,
+                                            node_stmt_exit *t_stmt) {
+  fprintf(t_file, "\texit(");
+  generate_expr(t_file, t_stmt->status);
+  fprintf(t_file, ");\n");
+}
+
+static inline void generate(const char *t_file_name, node_prg *t_prg) {
   const char *file_name = "out.c";
   if (t_file_name != nullptr) {
     file_name = t_file_name;
@@ -21,10 +39,10 @@ static inline void generator(const char *t_file_name, node_prg *prg) {
   FILE *file = fopen(file_name, "w");
   fprintf(file, "#include <stdio.h>\n");
   fprintf(file, "int main() {\n");
-  rda_for_each(it, (*prg)) {
+  rda_for_each(it, (*t_prg)) {
     switch (it->type) {
       case stmt_exit: {
-        generate_stmt_exit(file, &(it->value.stmt_exit));
+        generate_stmt_exit(file, &(it->value.exit_stmt));
         break;
       }
       default: {
