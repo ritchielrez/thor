602b5ae6e7a499d5471e58db4449d0a055509414
RitchielRez <ritchielrez@gmail.com>
2025-12-30 12:46:35 -0500

    feat(parser): implement variable declartion parsing and remove internal function definitions and declartions from header

--- include/parser.h
+++ include/parser.h
@@ -7,6 +7,7 @@
 
 #include "defines.h"
 #include "libraries/rit_dyn_arr.h"
+#include "libraries/rit_str.h"
 #include "tokenizer.h"
 
 typedef enum {
@@ -17,13 +18,13 @@ typedef enum {
   bp_div,
   bp_primary
 } binding_power;
-typedef enum { stmt_exit } node_stmt_type;
-typedef enum { expr_num, expr_bin } node_expr_type;
+typedef enum { stmt_exit, stmt_var_decl } node_stmt_type;
+typedef enum { expr_num, expr_var, expr_bin } node_expr_type;
 
 typedef struct node_expr node_expr;
 
 typedef struct {
-  int64_t num;
+  int64_t value;
 } node_num_expr;
 
 typedef struct {
@@ -36,6 +37,7 @@ struct node_expr {
   union {
     node_num_expr num_expr;
     node_bin_expr bin_expr;
+    rsv var_expr;
   } value;
   node_expr_type type;
 };
@@ -45,8 +47,14 @@ typedef struct {
 } node_stmt_exit;
 
 typedef struct {
+  rsv name;
+  node_expr *expr;
+} node_stmt_var_decl;
+
+typedef struct {
   union {
     node_stmt_exit exit_stmt;
+    node_stmt_var_decl var_decl_stmt;
   } value;
   node_stmt_type type;
 } node_stmt;
@@ -64,143 +72,7 @@ typedef struct {
   parser_t t_parser_name = parser_init(t_file)
 
 parser_t parser_init(const char *t_file);
-/// @internal
-INTERNAL_DEF node_expr *parse_expr(parser_t *t_parser,
-                                   binding_power t_binding_power);
-/// @internal
-INTERNAL_DEF inline node_expr *parse_num_expr(parser_t *t_parser);
-/// @internal
-INTERNAL_DEF inline node_expr *parse_bin_expr(parser_t *t_parser,
-                                              node_expr *t_lhs);
-/// @internal
-INTERNAL_DEF bool parse_stmt_exit(parser_t *t_parser);
-/// @internal
-INTERNAL_DEF bool parse_stmt(parser_t *t_parser);
 void parse(parser_t *t_parser);
 void parser_deinit(parser_t *t_parser);
-
-/// @internal
-INTERNAL_DEF inline token_t parser_peek(parser_t *t_parser, int64_t t_offset) {
-  return rda_at(t_parser->tokenizer->tokens, t_parser->idx + t_offset);
-}
-
-/// @internal
-INTERNAL_DEF inline token_t parser_consume(parser_t *t_parser) {
-  token_t tok = rda_at(t_parser->tokenizer->tokens, t_parser->idx);
-  t_parser->idx++;
-  return tok;
-}
-
-#define TOK_LINE parser_peek(t_parser, -1).line
-#define TOK_COL \
-  parser_peek(t_parser, -1).col + rsv_size(parser_peek(t_parser, -1).value)
-
-/// @internal
-/// @brief Look for a statement terminator token.
-///
-/// Look for it until one is found and then consume that token.
-/// Thus the parser will be at a new statment after this function
-/// is finished executing.
-INTERNAL_DEF inline void parser_skip_statement(parser_t *t_parser) {
-  while (parser_peek(t_parser, 0).type != token_semicolon &&
-         parser_peek(t_parser, 0).type != token_newline) {
-    parser_consume(t_parser);
-  }
-  parser_consume(t_parser);
-}
-
-/// @internal
-/// @brief Try consuming a token of specific type.
-///
-/// If not found, return a `token_invalid`.
-INTERNAL_DEF inline token_t parser_try_consume(parser_t *t_parser,
-                                               token_type t_token_type) {
-  if (parser_peek(t_parser, 0).type != t_token_type) {
-    return (token_t){.type = token_invalid,
-                     .value = {.m_size = 0, .m_str = ""},
-                     .line = parser_peek(t_parser, 0).line,
-                     .col = parser_peek(t_parser, 0).col};
-  }
-  return parser_consume(t_parser);
-}
-
-/// @internal
-/// @brief Look for a token of specific type.
-///
-/// If not found, print an error and skip over until a newline, semicolon or
-/// EOF is found.
-INTERNAL_DEF inline token_t parser_expected_consume(parser_t *t_parser,
-                                                    token_type t_token_type) {
-  token_t tok = parser_try_consume(t_parser, t_token_type);
-  if (tok.type == token_invalid) {
-    fprintf(stderr, "Error:%zu:%zu: expected %s\n", TOK_LINE, TOK_COL,
-            token_type_to_str(t_token_type));
-    parser_skip_statement(t_parser);
-    return (token_t){.type = token_error,
-                     .value = {.m_size = 0, .m_str = ""},
-                     .line = parser_peek(t_parser, 0).line,
-                     .col = parser_peek(t_parser, 0).col};
-  }
-  return tok;
-}
-
-/// @internal
-INTERNAL_DEF inline binding_power binding_power_lookup(
-    token_type t_token_type) {
-  switch (t_token_type) {
-    case token_num:
-      return bp_primary;
-    case token_add:
-      return bp_add;
-    case token_sub:
-      return bp_sub;
-    case token_mul:
-      return bp_mul;
-    case token_div:
-      return bp_div;
-    default: {
-      return bp_default;
-    }
-  }
-}
-
-#ifdef DEBUG
-/// @internal
-INTERNAL_DEF inline void print_expr(const char *t_prefix, node_expr *t_expr) {
-  switch (t_expr->type) {
-    case expr_num: {
-      printf("[DEBUG] %s: %I64d\n", t_prefix, t_expr->value.num_expr.num);
-      break;
-    }
-    case expr_bin: {
-#define PREFIX_SZ 64
-      char prefix[PREFIX_SZ];
-      int written_chars = snprintf(prefix, PREFIX_SZ, "%s.lhs", t_prefix);
-      if (written_chars >= PREFIX_SZ) {
-        fprintf(stderr,
-                "[DEBUG] Error: prefix needs to be a bigger buffer, file: %s, "
-                "line: %u",
-                __FILE__, __LINE__);
-        exit(1);
-      }
-      print_expr(prefix, t_expr->value.bin_expr.lhs);
-
-      printf("[DEBUG] %s.op: %s\n", t_prefix,
-             token_type_to_str(t_expr->value.bin_expr.op));
-
-      written_chars = snprintf(prefix, PREFIX_SZ, "%s.rhs", t_prefix);
-      if (written_chars >= PREFIX_SZ) {
-        fprintf(stderr,
-                "[DEBUG] Error: prefix needs to be a bigger buffer, file: %s, "
-                "line: %u",
-                __FILE__, __LINE__);
-        exit(1);
-      }
-      print_expr(prefix, t_expr->value.bin_expr.rhs);
-      break;
-    }
-  }
-}
-#endif  // DEBUG
 
 #endif  // PARSER_H_INCLUDED
--- src/parser.c
+++ src/parser.c
@@ -33,25 +33,145 @@ parser_t parser_init(const char *t_file) {
   return ret;
 }
 
-INTERNAL_DEF node_expr *parse_expr(parser_t *t_parser,
-                                   binding_power t_binding_power) {
-  node_expr *expr = parse_num_expr(t_parser);
-  while (binding_power_lookup(parser_peek(t_parser, 0).type) >
-         t_binding_power) {
-    expr = parse_bin_expr(t_parser, expr);
+INTERNAL_DEF token_t parser_peek(parser_t *t_parser, int64_t t_offset) {
+  return rda_at(t_parser->tokenizer->tokens, t_parser->idx + t_offset);
+}
+
+INTERNAL_DEF token_t parser_consume(parser_t *t_parser) {
+  token_t tok = rda_at(t_parser->tokenizer->tokens, t_parser->idx);
+  t_parser->idx++;
+  return tok;
+}
+
+#define TOK_LINE parser_peek(t_parser, -1).line
+#define TOK_COL \
+  parser_peek(t_parser, -1).col + rsv_size(parser_peek(t_parser, -1).value)
+
+INTERNAL_DEF void parser_skip_statement(parser_t *t_parser) {
+  while (parser_peek(t_parser, 0).type != token_semicolon &&
+         parser_peek(t_parser, 0).type != token_newline) {
+    parser_consume(t_parser);
   }
+  parser_consume(t_parser);
+}
+
+INTERNAL_DEF token_t parser_try_consume(parser_t *t_parser,
+                                        token_type t_token_type) {
+  if (parser_peek(t_parser, 0).type != t_token_type) {
+    return (token_t){.type = token_invalid,
+                     .value = {.m_size = 0, .m_str = ""},
+                     .line = parser_peek(t_parser, 0).line,
+                     .col = parser_peek(t_parser, 0).col};
+  }
+  return parser_consume(t_parser);
+}
+
+INTERNAL_DEF token_t parser_expected_consume(parser_t *t_parser,
+                                             token_type t_token_type) {
+  token_t tok = parser_try_consume(t_parser, t_token_type);
+  if (tok.type == token_invalid) {
+    fprintf(stderr, "Error:%zu:%zu: expected %s\n", TOK_LINE, TOK_COL,
+            token_type_to_str(t_token_type));
+    parser_skip_statement(t_parser);
+    return (token_t){.type = token_error,
+                     .value = {.m_size = 0, .m_str = ""},
+                     .line = parser_peek(t_parser, 0).line,
+                     .col = parser_peek(t_parser, 0).col};
+  }
+  return tok;
+}
+
+INTERNAL_DEF binding_power binding_power_lookup(token_type t_token_type) {
+  switch (t_token_type) {
+    case token_num:
+      return bp_primary;
+    case token_plus:
+      return bp_add;
+    case token_minus:
+      return bp_sub;
+    case token_star:
+      return bp_mul;
+    case token_fslash:
+      return bp_div;
+    default: {
+      return bp_default;
+    }
+  }
+}
+
+#ifdef DEBUG
+INTERNAL_DEF void print_expr(const char *t_prefix, node_expr *t_expr) {
+  switch (t_expr->type) {
+    case expr_num: {
+      printf("[DEBUG] %s: %I64d\n", t_prefix, t_expr->value.num_expr.value);
+      break;
+    }
+    case expr_var: {
+      printf("[DEBUG] %s: %s\n", t_prefix, rsv_get(t_expr->value.var_expr));
+      break;
+    }
+    case expr_bin: {
+#define PREFIX_SZ 64
+      char prefix[PREFIX_SZ];
+      int written_chars = snprintf(prefix, PREFIX_SZ, "%s.lhs", t_prefix);
+      if (written_chars >= PREFIX_SZ) {
+        fprintf(stderr,
+                "[DEBUG] Error: prefix needs to be a bigger buffer, file: %s, "
+                "line: %u",
+                __FILE__, __LINE__);
+        exit(1);
+      }
+      print_expr(prefix, t_expr->value.bin_expr.lhs);
+
+      printf("[DEBUG] %s.op: %s\n", t_prefix,
+             token_type_to_str(t_expr->value.bin_expr.op));
+
+      written_chars = snprintf(prefix, PREFIX_SZ, "%s.rhs", t_prefix);
+      if (written_chars >= PREFIX_SZ) {
+        fprintf(stderr,
+                "[DEBUG] Error: prefix needs to be a bigger buffer, file: %s, "
+                "line: %u",
+                __FILE__, __LINE__);
+        exit(1);
+      }
+      print_expr(prefix, t_expr->value.bin_expr.rhs);
+      break;
+    }
+  }
+}
+#endif  // DEBUG
+
+INTERNAL_DEF node_expr *parse_primary_expr(parser_t *t_parser) {
+  // token_t tok = parser_expected_consume(t_parser, token_num);
+  node_expr *expr = arena_alloc_struct(t_parser->allocator->m_ctx, node_expr);
+  switch (parser_peek(t_parser, 0).type) {
+    case token_num: {
+      token_t tok = parser_consume(t_parser);
+      node_num_expr num_expr = {.value = atoi(rsv_get(tok.value))};
+      expr->type = expr_num;
+      expr->value.num_expr = num_expr;
+      break;
+    }
+    case token_ident: {
+      token_t tok = parser_consume(t_parser);
+      rsv var_expr = tok.value;
+      expr->type = expr_var;
+      expr->value.var_expr = var_expr;
+      break;
+    }
+    default: {
+      fprintf(stderr, "invalid token type\n");
+      break;
+    }
+  }
+  // if (tok.type == token_error) exit(1);
   return expr;
 }
 
-INTERNAL_DEF node_expr *parse_num_expr(parser_t *t_parser) {
-  token_t tok = parser_expected_consume(t_parser, token_num);
-  if (tok.type == token_error) exit(1);
-  node_num_expr num_expr = {.num = atoi(rsv_get(tok.value))};
-  node_expr *expr = arena_alloc_struct(t_parser->allocator->m_ctx, node_expr);
-  expr->type = expr_num;
-  expr->value.num_expr = num_expr;
-  return expr;
-}
+// Forward declare because `parse_bin_expr()` and `parse_expr()` rely on each
+// other.
+INTERNAL_DEF node_expr *parse_expr(parser_t *t_parser,
+                                   binding_power t_binding_power);
 
 INTERNAL_DEF node_expr *parse_bin_expr(parser_t *t_parser, node_expr *t_lhs) {
   node_expr *expr = arena_alloc_struct(t_parser->allocator->m_ctx, node_expr);
@@ -60,6 +180,16 @@ INTERNAL_DEF node_expr *parse_bin_expr(parser_t *t_parser, node_expr *t_lhs) {
   expr->value.bin_expr.op = parser_consume(t_parser).type;
   expr->value.bin_expr.rhs =
       parse_expr(t_parser, binding_power_lookup(expr->value.bin_expr.op));
+  return expr;
+}
+
+INTERNAL_DEF node_expr *parse_expr(parser_t *t_parser,
+                                   binding_power t_binding_power) {
+  node_expr *expr = parse_primary_expr(t_parser);
+  while (binding_power_lookup(parser_peek(t_parser, 0).type) >
+         t_binding_power) {
+    expr = parse_bin_expr(t_parser, expr);
+  }
   return expr;
 }
 
@@ -86,6 +216,30 @@ INTERNAL_DEF bool parse_stmt_exit(parser_t *t_parser) {
   return true;
 }
 
+INTERNAL_DEF bool parse_stmt_var_decl(parser_t *t_parser,
+                                      token_t t_token_ident) {
+  if (parser_expected_consume(t_parser, token_colon).type == token_error) {
+    return false;
+  }
+  if (parser_expected_consume(t_parser, token_assignment).type == token_error) {
+    return false;
+  }
+  node_expr *expr = parse_expr(t_parser, bp_default);
+  if (parser_peek(t_parser, 0).type != token_semicolon &&
+      parser_peek(t_parser, 0).type != token_newline) {
+    fprintf(stderr, "Error:%zu:%zu: expected a newline or ;\n", TOK_LINE,
+            TOK_COL);
+    return false;
+  }
+  parser_consume(t_parser);  // Consume the semicolon or newline
+  node_stmt stmt;
+  stmt.type = stmt_var_decl;
+  stmt.value.var_decl_stmt.name = t_token_ident.value;
+  stmt.value.var_decl_stmt.expr = expr;
+  rda_push_back(t_parser->prg, stmt, t_parser->allocator);
+  return true;
+}
+
 INTERNAL_DEF bool parse_stmt(parser_t *t_parser) {
   if (parser_peek(t_parser, 0).type == token_newline ||
       parser_peek(t_parser, 0).type == token_semicolon) {
@@ -93,6 +247,15 @@ INTERNAL_DEF bool parse_stmt(parser_t *t_parser) {
     return true;
   } else if (parser_try_consume(t_parser, token_exit).type != token_invalid) {
     return parse_stmt_exit(t_parser);
+  } else if (parser_peek(t_parser, 0).type == token_ident) {
+    return parse_stmt_var_decl(t_parser, parser_consume(t_parser));
+  } else if (parser_try_consume(t_parser, token_fslash).type != token_invalid &&
+             parser_try_consume(t_parser, token_fslash).type != token_invalid) {
+    while (parser_peek(t_parser, 0).type != token_newline) {
+      parser_consume(t_parser);
+    }
+    parser_consume(t_parser);
+    return true;
   } else {
     fprintf(stderr, "Error:%zu:%zu: invalid identifier %s\n",
             parser_peek(t_parser, 0).line, parser_peek(t_parser, 0).col,
@@ -111,8 +274,16 @@ void parse(parser_t *t_parser) {
 #ifdef DEBUG
   rda_for_each(it, t_parser->prg) {
     switch (it->type) {
-      case stmt_exit:
+      case stmt_exit: {
         print_expr("stmt_exit.status", it->value.exit_stmt.status);
+        break;
+      }
+      case stmt_var_decl: {
+        printf("[DEBUG] stmt_var_decl.name: %s\n",
+               rsv_get(it->value.var_decl_stmt.name));
+        print_expr("stmt_var_decl.expr", it->value.var_decl_stmt.expr);
+        break;
+      }
     }
   }
 #endif  // DEBUG
